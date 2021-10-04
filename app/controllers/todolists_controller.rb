03a8c69ef5d089b48c4a9f2ca3d60a0b865ae1b8
EC2 Default User <ec2-user@ip-172-31-85-2.ec2.internal>
2021-10-04 07:16:47 +0000

    [start]rails-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -22,6 +22,10 @@ class TodolistsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def edit
+    @list = List.find(params[:id])
+  end
+  
   private
   #ストロングパラメータ
   def list_params
